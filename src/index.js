dbe3ce2adeaa0f87d0362534e6671c8e58a52553
crigigal <50228889+crigigal@users.noreply.github.com>
2021-07-10 14:44:51 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>My own Websites</h1>
+    <ul>
+      <li>Sapsapiens</li>
+      <li>GoGuiri</li>
+      <li>CristinaGallo</li>
+      <li>Trasteros</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
